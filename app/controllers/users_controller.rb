fc3331bb49aa2ef1f2932a5d7fa48f09fdb24f42
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-09 13:42:05 -0500

    add sending email with name feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,4 +28,29 @@ class UsersController < ApplicationController
     render json: { message: "User has been deleted" }
   end
 
+  def create
+    @user = User.new(params[:user])
+ 
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome Email after save
+        UserMailer.welcome_email(@user).deliver
+ 
+        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
+        format.json { render :json => @user, :status => :created, :location => @user }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def welcome_email(user)
+    @user = user
+    email_with_name = "#{@user.name} <#{@user.email}>"
+    mail(:to => email_with_name, :subject => "Welcome to My Awesome Site")
+  end
+  
 end
+
+
